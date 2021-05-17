a28a5fe1c95f04976ffa2639295c92cf5c14bbed
11382chagar <83670860+11382chagar@users.noreply.github.com>
2021-05-17 05:52:52 +0100

    Delete Password.py

--- Password.py
+++
@@ -1,8 +0,0 @@
-USER = '11382chagar'
-PASSWORD = 'CASuals2'
-user_attempt = input('Please enter your username ')
-password_attempt = input('Please enter your password ')
-if password_attempt == PASSWORD and user_attempt == USER:
-    print('Access Granted')
-else:
-    print('Access Denied')
